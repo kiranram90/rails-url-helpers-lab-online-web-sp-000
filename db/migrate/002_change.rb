869be7d5781e376a8c9db75a4b2e8a23e1bbb6a0
kiran ram <kram@lions.molloy.edu>
2021-01-13 04:49:42 +0000

    Automatically backed up by Learn

--- db/migrate/002_change.rb
+++ db/migrate/002_change.rb
@@ -1,10 +1,4 @@
 class CreateStudents < ActiveRecord::Migration
   def change
-    create_table :students do |t|
-      t.string :first_name
-      t.string :last_name
-
-      t.timestamps null: false
-    end
+    add_column :students, :active, :boolean, default: false
   end
-end
